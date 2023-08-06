9c11661ad89a100c86466225bbd5d7352b23ad71
Aly Abd el shafy <alyaboudaboud@gmail.com>
2023-08-06 13:11:02 +0300

    Add more examples about lits

--- Lists/Lists.py
+++ Lists/Lists.py
@@ -1,24 +1,54 @@
-# create list
-bicycles = ["trek", "cannodndale", "redline", "speciaized"]
-print(bicycles)
 
-#print element
-print(bicycles[0])
+motorcycles = ['honda', 'yamaha', 'suziki']
+#print(motorcycles)
 
-#use string methods in array
-#title method make string starts with caps
-print(bicycles[0].title())
+motorcycles[0] = 'ducati'
+#print(motorcycles)
 
-#return last element in the list
-print(bicycles[-1])
+motorcycles.append('honda')
+#print(motorcycles)
 
-#When use negative index it means you want that inded from the end
-print(bicycles[-2].title())
 
-#Applying string concation with array items
+#Remove element from list
+del motorcycles[0]
+#print(motorcycles)
 
-message = "My first bicycle was a "+ bicycles[0].title()+ "."
-print(message)
+#remove element from list
+del motorcycles[1]
+#print(motorcycles)
+
+# pop last element
+motorcycles = ['honda', 'yamaha', 'suziki']
+popped_moto = motorcycles.pop()
+#print(motorcycles)
+#print(popped_moto)
+
+#pop element at position
+motorcycles = ['honda', 'yamaha', 'suziki']
+popped_moto = motorcycles.pop(1)
+#print(popped_moto)
+
+#use del when you want to remove item and you don't need the value
+#use pop when you want to remove item and you want work on item
+
+
+
+#Remove item by value
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("\nA "+ too_expensive.title() + " is too expensive")
+
+
+
+
+
+
+
+
+
 
 
 
