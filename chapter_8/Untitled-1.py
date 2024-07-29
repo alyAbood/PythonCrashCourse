fb77d40ec2f42f99fa91938d59ec984d3278d4d2
Aly Abd el shafy <alyaboudaboud@gmail.com>
2024-07-29 19:33:36 +0300

    Return dictionary

--- chapter_8/Untitled-1.py
+++ chapter_8/Untitled-1.py
@@ -1,23 +1,70 @@
-
-def describe_pet(animal_type, pet_name ='harry'):
-    print(f"i have a {animal_type}")
-    print(f"My {animal_type}'s name is {pet_name}")
-
-describe_pet("dog")
-describe_pet(animal_type="bird", pet_name="lara")
-
-#Exercise 8.3
-def make_shirt(message, size = "large" ):
-    print(f"The size of the shirt is {size} and message is {message}")
-
-make_shirt("Hello to my world","xl")
-make_shirt(message="Python rocks")
+def get_formatted_name(first_name, last_name, middle_name =''):
+    full_name = ""
+    if middle_name:
+        full_name  = (f"{first_name} {middle_name} {last_name}")
+    else:
+        full_name = (f"{first_name} {last_name}")
+    return full_name.title()
 
 
-def get_formatted_name(first_name, last_name):
-    """Return full name , neatly formatted"""
-    full_name = f"{first_name} {last_name}"
-    return full_name
+print(get_formatted_name("aly","aboud"))
+print(get_formatted_name("aly", "aboud", "abdelshafy"))
 
-full_name = get_formatted_name("aly","aboud")
-print(full_name.title())
+
+def build_person(first_name, last_name):
+    person = {'first':first_name,'last' :last_name}
+    return person
+
+musician = build_person('jimi', 'hendrix')
+print(musician)
+
+def build_person_1(first_name, last_name, age = None):
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+
+    return person
+
+print(build_person_1('aly','aboud', 30))
+
+def enter_your_age():
+    age_input = input("Please enter your age (optional): ")
+    age = 0
+    if age_input:
+        try:
+            age = int(age_input)
+            print(f"this is age {age}")
+        except:
+            print("you entered a wrong format age")
+            enter_your_age()
+    return age
+
+print("tell us more about you")
+f_name = input("First name: ")
+l_name = input("Last namea: ")
+age = enter_your_age()
+print(age)
+print(build_person_1(first_name=f_name, last_name= l_name, age = age))
+
+
+
+def enter_string(message): 
+    user_input = input(message)
+    return user_input
+
+
+def city_country(city_name, country_name):
+    print(f"{city_name}, {country_name}")
+
+for i in range(3):
+    print("enter 'q' at any time to quit")
+    city_name = enter_string("Please enter city name: ")
+    if(city_name == 'q'):
+        break
+    country_name = enter_string("Please enter country name: ")
+    if (country_name == 'q'):
+        break
+    city_country(city_name= city_name, country_name= country_name)
+
+        
+
