0c747a8c37b13fd8d468db790ae7ae7794c04715
Aly Abd el shafy <alyaboudaboud@gmail.com>
2024-08-02 18:59:53 +0300

    Adding exercise

--- chapter_8/Untitled-1.py
+++ chapter_8/Untitled-1.py
@@ -13,3 +13,22 @@ def build_profile(first, last, **user_info):
     return user_info
 user_profile = build_profile("aly","aboud",location = "alexandria", field = "physics")
 print(user_profile)
+
+
+def extras_on_sandwishes(*toppings):
+    for topping in toppings:
+        print(f"i have added {topping}")
+    print("i finished all topping on my sandwish\n")
+
+extras_on_sandwishes("pasta","chicken", "white sauce")
+extras_on_sandwishes("meat","red sauce","vegetables")
+extras_on_sandwishes("meat", "soap", "chicken", "fries")
+
+
+def build_car(manufacturer, model_name, **car_info):
+    car_info["manufacturer"] = manufacturer
+    car_info["model_name"] = model_name
+    return car_info
+
+car = build_car('subaru', 'outback', color='blue', tow_package=True)
+print(car)
