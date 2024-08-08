09e0b20bc843024ee8e70e84cca2f4015d411316
Aly Abd el shafy <alyaboudaboud@gmail.com>
2024-08-08 15:24:06 +0300

    add range method in the battery

--- chapter_9/Car.py
+++ chapter_9/Car.py
@@ -48,6 +48,14 @@ class Battery:
     def describe_battery(self):
         """print a statement describing the battery size"""
         print(f"This car has a {self.batter_size}-KWH battery.")
+    
+    def get_range(self):
+        """Print a statment about the range this battery provides"""
+        if self.batter_size == 40:
+            range = 150
+        elif self.batter_size == 65:
+            range = 225
+        print(f"This car can go about {range} miles on a full charge.")
         
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vechiles"""
